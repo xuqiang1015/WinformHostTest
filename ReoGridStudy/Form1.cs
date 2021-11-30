7f539d1185fcfd0fc640a716862ad7b8438c4a9d
xq <xuqiango@173.com>
2021-11-30 16:14:13 +0800

    # Make ReoGridStudy project DPI Aware

--- ReoGridStudy/Form1.cs
+++ ReoGridStudy/Form1.cs
@@ -30,7 +30,7 @@ namespace ReoGridStudy
         public Form1()
         {
             InitializeComponent();
-            LoadExcel();
+            //LoadExcel();
         }
 
         private void m_gridCtrl_Click(object sender, EventArgs e)
